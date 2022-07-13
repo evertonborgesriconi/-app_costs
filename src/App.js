8c77e58f4414e6db96d766c82275e8c48ff438c4
Everton Riconi <evertonriconi@gmail.com>
2022-07-13 16:51:57 -0300

    subindo o esqueleto do projeto
    subindo o esqueleto do projeto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Company from "./components/pages/Company";
+import Contact from "./components/pages/Contact";
+import Home from "./components/pages/Home";
+import NewProject from "./components/pages/NewProject";
+
+import Container from "./components/layout/Container";
+import Navbar from "./components/layout/Navbar";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar/>
+      <Container>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/company" element={<Company />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/newproject" element={<NewProject />} />
+        </Routes>
+      </Container>
+      <p>Footer</p>
     </div>
   );
 }
---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <div>
+      <Link to="/">Home</Link>
+      <Link to="/company">Company</Link>
+      <Link to="/contact">Contact</Link>
+      <Link to="/newproject">NewProject</Link>
+    </div>
+  );
+}
+export default Navbar
