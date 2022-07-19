67b610c4a9376c671cd1112c5713258629b7f636
Everton Riconi <evertonriconi@gmail.com>
2022-07-19 16:45:28 -0300

    Criando o formulario de cadastro

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,15 +1,37 @@
-import styles  from "./NewProject.module.css";
+import ProjectForm from "../project/ProjectForm";
+import styles from "./NewProject.module.css";
+
+import { useNavigate } from "react-router-dom";
 
 function NewProject() {
+  const navigate = useNavigate();
 
-    return (
-        <div className={styles.newproject_container}>
-            <h1>Criar Projeto</h1>
-            <p> Crie seu projeto para depois adicionar os serviços</p>
+  function createPost(project) {
+    project.cost = 0;
+    project.services = [];
 
-        </div>
-    )
-    
+    fetch("http://localhost:5000/projects", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(project),
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        console.log(data);
+        navigate('/projects')
+      })
+      .catch((err) => console.log(err));
+  }
+
+  return (
+    <div className={styles.newproject_container}>
+      <h1>Criar Projeto</h1>
+      <p> Crie seu projeto para depois adicionar os serviços</p>
+      <ProjectForm handleSubmit={createPost} btnText="Criar Projeto" />
+    </div>
+  );
 }
 
-export default NewProject
+export default NewProject;
